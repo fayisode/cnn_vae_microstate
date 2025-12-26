da3f65b8ec4c51e3c183af2609bf98ca3a1ad216
Saheed Faremi <37019902+fayisode@users.noreply.github.com>
2025-12-26 08:44:16 +0000

    Update: Set GPU constraint in Code Running

--- train.py
+++ train.py
@@ -308,7 +308,7 @@ class TrainingOrchestrator:
 
     def _get_param_ranges(self) -> List[Tuple[int, int, int]]:
         """Determine ranges for n_channels and latent_dim"""
-        cluster_range = [3, 20]
+        cluster_range = [3]
         batch_size_range = [128]
         latent_dim_range = [32]
         param_combinations = list(
